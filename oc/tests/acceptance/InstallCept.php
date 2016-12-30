4c79a1076cef2f236f94931242905240a02f5300
kotsios5 <pantazis.constantinos@gmail.com>
2016-12-30 16:29:51 +0000

    submodule oc/common

--- oc/tests/acceptance/InstallCept.php
+++ oc/tests/acceptance/InstallCept.php
@@ -5,11 +5,11 @@ $I->am('the admin');
 $I->wantTo('install Yclas');
 $I->lookForwardTo('see a success message after the installation and see the website');
 
-$I->amOnPage('/?LANGUAGE=en_US'); //select language already
+$I->amOnPage('/install-yclas.php'); //select language already
 $I->see('Welcome to the super easy and fast installation');
 
 $I->click('Start installation');
-$I->see('DB Configuration');
+$I->see('Software Requirements');
 
 
 
