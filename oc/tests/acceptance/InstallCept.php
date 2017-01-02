5a6353eb4bbceb3e75d27392773373597b13f6f0
kotsios5 <pantazis.constantinos@gmail.com>
2017-01-02 16:26:26 +0000

    install test - automated install

--- oc/tests/acceptance/InstallCept.php
+++ oc/tests/acceptance/InstallCept.php
@@ -8,21 +8,21 @@ $I->lookForwardTo('see a success message after the installation and see the webs
 $I->amOnPage('/'); //select language already
 $I->see('Welcome to the super easy and fast installation');
 
-$I->click('Start Installation');
-sleep(2);
-$I->fillField('#DB_HOST', 'localhost');
-$I->fillField('#DB_NAME', 'openclassifieds');
-$I->fillField('#DB_USER', 'root'); 	# Default travis user: root or travis
-$I->fillField('#DB_USER', ''); 		# Default travis pass (blank): 
+$I->see('Start Installation','button');
+// sleep(2);
+// $I->fillField('#DB_HOST', 'localhost');
+// $I->fillField('#DB_NAME', 'openclassifieds');
+// $I->fillField('#DB_USER', 'root'); 	# Default travis user: root or travis
+// $I->fillField('#DB_USER', ''); 		# Default travis pass (blank): 
 
-$I->click('Continue');
+// $I->click('Continue');
 
-$I->see('Site Configuration');
-$I->fillField('#SITE_NAME', 'Test Yclas');
-$I->fillField('#ADMIN_EMAIL', 'admin@reoc.lo');
-$I->fillField('#ADMIN_PWD', '1234');
+// $I->see('Site Configuration');
+// $I->fillField('#SITE_NAME', 'Test Yclas');
+// $I->fillField('#ADMIN_EMAIL', 'admin@reoc.lo');
+// $I->fillField('#ADMIN_PWD', '1234');
 
-$I->click('Install');
+// $I->click('Install');
 
 
 $I->see('Congratulations');
