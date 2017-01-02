fe85cfc4d5365183ef0dbc0b9a2857a57ac1c9b6
kotsios5 <pantazis.constantinos@gmail.com>
2017-01-02 13:48:47 +0000

    requirements

--- build/php5-6.sh
+++ build/php5-6.sh
@@ -1,3 +1,10 @@
+# Upgrade PHP from 5.5.9 to 5.6
+sudo add-apt-repository ppa:ondrej/php -y > /dev/null
+sudo apt-get -y update
+sudo apt-get -y install php5.6 php5.6-mcrypt php5.6-mbstring php5.6-curl php5.6-cli php5.6-mysql php5.6-gd php5.6-zip
+sudo service apache2 restart
+php -v
+
 sudo apt-get install apache2 libapache2-mod-fastcgi
 sudo apt-get install apache2 php5 libapache2-mod-php5
 
@@ -6,34 +13,17 @@ sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php5/apache2/php
 sudo service apache2 restart
 
 # enable php-fpm
-sudo apt-get install php5-fpm -y > /dev/null
+sudo apt-get install php5.6-fpm -y > /dev/null
 sudo service apache2 restart
   
 sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php5/fpm/php.ini
 sudo service apache2 restart
 
-# install php5-curl
-sudo apt-get install php5-curl
-
 # install gettext
 sudo apt-get install gettext
 sudo service apache2 restart
 
-# Install mcrypt
-sudo apt-get install php5-mcrypt
-# - sudo mv -i /etc/php5/apache2/conf.d/20-mcrypt.ini /etc/php5/mods-available/
+# Enable mcrypt
 sudo php5enmod mcrypt
 sudo service apache2 restart
 
-# Install GD
-sudo apt-get install php5-gd
-sudo service apache2 restart
-
-# MySQLi extension
-sudo apt-get install php5-mysql
-sudo service apache2 restart
-
-# Upgrade PHP from 5.5.9 to 5.6
-sudo add-apt-repository ppa:ondrej/php -y > /dev/null
-sudo apt-get update
-sudo apt-get install php5
