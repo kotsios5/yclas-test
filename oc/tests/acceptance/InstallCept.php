acacba3027e1949c7ed606f594fb6e73677401cb
kotsios5 <pantazis.constantinos@gmail.com>
2017-01-02 15:22:01 +0000

    install test - automated install

--- oc/tests/acceptance/InstallCept.php
+++ oc/tests/acceptance/InstallCept.php
@@ -5,7 +5,7 @@ $I->am('the admin');
 $I->wantTo('install Yclas');
 $I->lookForwardTo('see a success message after the installation and see the website');
 
-$I->amOnPage('/?LANGUAGE=en_US'); //select language already
+$I->amOnPage('/'); //select language already
 $I->see('Welcome to the super easy and fast installation');
 
 $I->click('Start installation');
