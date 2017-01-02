b847266003c17d3973838dd6688247c7cc120e7a
kotsios5 <pantazis.constantinos@gmail.com>
2017-01-02 14:04:00 +0000

    requirements

--- build/php5-6.sh
+++ build/php5-6.sh
@@ -8,15 +8,15 @@ php -v
 sudo apt-get install apache2 libapache2-mod-fastcgi
 sudo apt-get install apache2 php5 libapache2-mod-php5
 
-sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php5/cli/php.ini
-sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php5/apache2/php.ini
+sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/5.6/cli/php.ini
+sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/5.6/apache2/php.ini
 sudo service apache2 restart
 
 # enable php-fpm
 sudo apt-get install php5.6-fpm -y > /dev/null
 sudo service apache2 restart
   
-sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php5/fpm/php.ini
+sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/5.6/fpm/php.ini
 sudo service apache2 restart
 
 # install gettext
