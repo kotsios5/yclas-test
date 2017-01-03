d5446bc219c8ba9dda99ec3afd0dfbba054a8ab8
kotsios5 <pantazis.constantinos@gmail.com>
2017-01-03 14:13:29 +0000

    php7

--- build/php7.sh
+++ build/php7.sh
@@ -1,2 +1,35 @@
 php -v
-sudo apt-get install libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json
+sudo apt-add-repository ppa:ondrej/php -y
+sudo apt-get update
+sudo apt-get install -y php7.0 -y
+
+sudo apt-get install php7.0-curl -y
+
+sudo apt-get install php7.0-apc -y
+
+sudo apt-get install php7.0-gd -y
+
+sudo apt-get install php7.0-fpm -y
+sudo a2enmod proxy_fcgi setenvif
+sudo service apache2 restart
+sudo a2enconf php7.0-fpm
+sudo service apache2 reload
+
+sudo apt-get install php7.0-mbstring -y
+
+sudo apt-get install php7.0-mcrypt -y
+
+sudo apt-get install php7.0-mysql -y
+
+sudo apt-get install php7.0-xml -y
+
+sudo apt-get install php7.0-zip -y
+
+sudo apt-get install php-soap -y
+
+sudo service apache2 restart
+
+sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/7.0/cli/php.ini
+sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/7.0/fpm/php.ini
+sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/7.0/apache2/php.ini
+sudo service apache2 restart
