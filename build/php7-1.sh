aad65c3e86902eb7a81ef996ba5ed7a0bbea6949
kotsios5 <pantazis.constantinos@gmail.com>
2017-01-04 13:03:51 +0000

    php7.1

--- build/php7-1.sh
+++ build/php7-1.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -y language-pack-en-base
-sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php
+sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
 sudo apt-get install php7.1
 sudo apt-get install php7.1-mysql
