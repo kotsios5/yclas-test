0716cd6b9374f116a84b0e1e0ef26c195d45c78e
kotsios5 <pantazis.constantinos@gmail.com>
2017-01-06 14:44:43 +0000

    install

--- build/install/install-yclas.sh
+++ build/install/install-yclas.sh
@@ -1,6 +1,7 @@
 # Automated installation
 
 # Move files that installation should create
+# remove install/install.lock
 # Import database with admin user and 4 ads
 
 sudo cp /var/www/yclas-test/build/install/auth.php /var/www/yclas-test/oc/config/
@@ -8,3 +9,4 @@ sudo cp /var/www/yclas-test/build/install/database.php /var/www/yclas-test/oc/co
 sudo cp /var/www/yclas-test/build/install/robots /var/www/yclas-test/robots.txt
 sudo cp /var/www/yclas-test/build/install/sitemap /var/www/yclas-test/sitemap.xml
 sudo cp /var/www/yclas-test/build/install/htaccess-install /var/www/yclas-test/.htaccess
+sudo rm -f  /var/www/yclas-test/install/install.lock
