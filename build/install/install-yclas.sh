1c11b44e8dbd285e000c0cf5fe275ab9b00a53a1
kotsios5 <pantazis.constantinos@gmail.com>
2017-01-06 12:47:13 +0000

    install

--- build/install/install-yclas.sh
+++ build/install/install-yclas.sh
@@ -1,1 +1,6 @@
 sudo cp /var/www/yclas-test/build/install/auth.php /var/www/yclas-test/oc/config/
+sudo cp /var/www/yclas-test/build/install/database.php /var/www/yclas-test/oc/config/
+sudo cp /var/www/yclas-test/build/install/database.php /var/www/yclas-test/oc/config/
+sudo cp /var/www/yclas-test/build/install/robots.txt /var/www/yclas-test/
+sudo cp /var/www/yclas-test/build/install/sitemap.xml /var/www/yclas-test/
+sudo cp /var/www/yclas-test/build/install/.htaccess /var/www/yclas-test/
