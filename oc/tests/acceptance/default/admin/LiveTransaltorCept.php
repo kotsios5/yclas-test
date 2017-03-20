f595e3b480fb410fe8beb16e6ea9ed106e9c4980
kotsios5 <pantazis.constantinos@gmail.com>
2017-03-20 14:39:22 +0000

    test

--- oc/tests/acceptance/default/admin/LiveTransaltorCept.php
+++ oc/tests/acceptance/default/admin/LiveTransaltorCept.php
@@ -7,14 +7,14 @@ $I->login_admin();
 
 // default
 $I->amOnPage('/?edit_translation=1');
-$I->seeElement('.editable.editable-click');
+$I->seeElement('.editable');
 $I->amOnPage('/?edit_translation=0');
-$I->dontSeeElement('.editable.editable-click');
+$I->dontSeeElement('.editable');
 
 $I->activate_theme('basecamp_free');
 $I->amOnPage('/?edit_translation=1');
-$I->seeElement('.editable.editable-click');
+$I->seeElement('.editable');
 $I->amOnPage('/?edit_translation=0');
-$I->dontSeeElement('.editable.editable-click');
+$I->dontSeeElement('.editable');
 
 $I->activate_theme('default');
