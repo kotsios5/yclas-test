997b733c90ec382e02d26756a64b3eea685b61e1
kotsios5 <pantazis.constantinos@gmail.com>
2017-09-19 16:14:12 +0100

    update

--- themes/default/views/pages/user/profile.php
+++ themes/default/views/pages/user/profile.php
@@ -6,11 +6,24 @@
 
 <div id="user_profile_info" class="row">
     <div class="col-xs-3">
-        <a class="thumbnail">
-            <picture>
-                <img class="img-rounded img-responsive" src="<?=Core::imagefly($user->get_profile_image(),200,200)?>" alt="<?=$user->name?>">
-            </picture>
-        </a>
+        <?$images = $user->get_profile_images(); if ($images):?>
+            <div id="gallery">
+                <?$i = 0; foreach ($images as $key => $image):?>
+                    <a href="<?=$image?>" class="thumbnail gallery-item <?=$i > 0 ? 'hidden' : NULL?>" data-gallery>
+                        <img class="img-rounded img-responsive" src="<?=Core::imagefly($image,200,200)?>" alt="<?=$user->name?>">
+                    </a>
+                <?$i++; endforeach?>
+            </div>
+            <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
+                <div class="slides"></div>
+                <h3 class="title"></h3>
+                <a class="prev">‹</a>
+                <a class="next">›</a>
+                <a class="close">×</a>
+                <a class="play-pause"></a>
+                <ol class="indicator"></ol>
+            </div>
+        <?endif?>
     </div>
     <div class="col-xs-12 col-sm-9">
         <h3><?=$user->name?> <?=$user->is_verified_user();?></h3>
@@ -55,7 +68,7 @@
         </ul>
         <?if (Theme::get('premium')==1):?>
             <?if(isset($user->cf_whatsapp) AND $user->cf_whatsapp!=''):?>
-                <a href="intent://send/<?=$user->cf_whatsapp?>#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end" title="Chat with <?=$user->name?>" alt="Whatsapp"><i class="fa fa-2x fa-whatsapp" style="color:#43d854"></i></a>
+                <a href="https://api.whatsapp.com/send?phone=<?=$user->cf_whatsapp?>" title="Chat with <?=$user->name?>" alt="Whatsapp"><i class="fa fa-2x fa-whatsapp" style="color:#43d854"></i></a>
             <?endif?>
             <?if(isset($user->cf_skype) AND $user->cf_skype!=''):?>
                 <a href="skype:<?=$user->cf_skype?>?chat" title="Chat with <?=$user->name?>" alt="Skype"><i class="fa fa-2x fa-skype" style="color:#00aff0"></i></a>
